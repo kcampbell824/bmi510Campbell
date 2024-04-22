45063f159c1e3c458a1adb8b8dc20a219affa575
Kiersten Campbell <kierstencampbell21@gmail.com>
2024-04-22 14:44:03 -0400

    test script added, logLikBernoulli stable

---
+++ tests/test_functions.R
@@ -0,0 +1,30 @@
+# Test script for package functions
+# This mimics a new user installing the package and attempting to use functions
+# Also includes accuracy/functionality tests for each function
+
+# Load package from git
+devtools::install_git("https://github.com/kcampbell824/bmi510Campbell", branch = "dev")
+
+# Bernoulli log likelihood test
+true_p = 0.62
+x = rbinom(100, 1, true_p)
+obs_p = sum(x)/length(x)
+logLikBernoulli(x) == obs_p # Expect TRUE
+
+# Compare to using dbinom for grid search
+p_s = seq(0, 1, 0.001)
+mle_p = 0
+max_like = -1
+
+for(i in seq(1, length(p_s))){
+  logLik = sum(dbinom(x, 1, p_s[i], log = TRUE))
+  if(i == 1){
+    mle_p = p_s[i]
+    max_like = logLik
+  }
+  else if(logLik > max_like){
+    max_like = logLik
+    mle_p = p_s[i]
+  }
+}
+logLikBernoulli(x) == mle_p # Expect TRUE
